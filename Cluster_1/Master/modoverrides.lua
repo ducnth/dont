944fc11adec52daa459566c357bcd7f7e2c5af3a
duc.nguyen6 <duc.nguyen6@mservice.com,vn>
2021-12-20 09:47:17 +0700

    9h47

---
+++ Cluster_1/Master/modoverrides.lua
@@ -0,0 +1,89 @@
+return {
+  ["workshop-1207269058"]={ configuration_options={  }, enabled=true },
+  ["workshop-1530801499"]={
+    configuration_options={ Hunger_Cost=1, Ownership=false, Sanity_Cost=1 },
+    enabled=true 
+  },
+  ["workshop-1699194522"]={ configuration_options={ Skillbadge=true }, enabled=true },
+  ["workshop-1991746508"]={ configuration_options={ Language="A", ShowBuff=true }, enabled=true },
+  ["workshop-2156905460"]={
+    configuration_options={ shovel_loot_chance=0.04, shovel_repair_time=96 },
+    enabled=true 
+  },
+  ["workshop-2543193992"]={ configuration_options={ papermen=5, yuyan=true }, enabled=true },
+  ["workshop-347079953"]={
+    configuration_options={ DFV_Language="EN", DFV_MinimalMode="default" },
+    enabled=true 
+  },
+  ["workshop-375859599"]={
+    configuration_options={
+      divider=5,
+      random_health_value=0,
+      random_range=0,
+      send_unknwon_prefabs=false,
+      show_type=0,
+      unknwon_prefabs=1,
+      use_blacklist=true 
+    },
+    enabled=true 
+  },
+  ["workshop-378160973"]={
+    configuration_options={
+      ENABLEPINGS=true,
+      FIREOPTIONS=2,
+      OVERRIDEMODE=false,
+      SHAREMINIMAPPROGRESS=true,
+      SHOWFIREICONS=true,
+      SHOWPLAYERICONS=true,
+      SHOWPLAYERSOPTIONS=2 
+    },
+    enabled=true 
+  },
+  ["workshop-501385076"]={
+    configuration_options={
+      quick_cook_on_fire=true,
+      quick_harvest=true,
+      quick_pick_cactus=true,
+      quick_pick_plant_normal_ground=true 
+    },
+    enabled=true 
+  },
+  ["workshop-569043634"]={
+    configuration_options={
+      Health_Penalty_Generic="Unchanged",
+      Health_Penalty_Maximum="Unchanged",
+      Health_Penalty_Meat_Effigy="Unchanged",
+      Health_Penalty_Portal="Unchanged",
+      Health_Respawn_Amount="Unchanged",
+      KeepInventory="Unchanged",
+      NoPlayerSkeletons="Unchanged",
+      ReturnHotkey="Unchanged",
+      ReturnHotkey_Mode="Closest",
+      SpawnProtection="Unchanged",
+      campfire="Enabled",
+      coldfire="Enabled",
+      coldfirepit="Enabled",
+      firepit="Enabled",
+      reviver="Unchanged",
+      skeleton="Unchanged",
+      usetags="Unchanged" 
+    },
+    enabled=true 
+  },
+  ["workshop-625415718"]={ configuration_options={  }, enabled=true },
+  ["workshop-666155465"]={
+    configuration_options={
+      chestB=-1,
+      chestG=-1,
+      chestR=-1,
+      display_hp=-1,
+      food_estimation=-1,
+      food_order=0,
+      food_style=0,
+      lang="auto",
+      show_food_units=-1,
+      show_uses=-1 
+    },
+    enabled=true 
+  } 
+}
